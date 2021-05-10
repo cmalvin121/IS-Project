f2609c8a1636825b2fc00f9ebcb74991d2b6742b
R.Miseria <lukekhr.2049@gmail.com>
2021-05-10 15:26:59 +0800

    Hooked up firebase

--- ChatRoom/src/views/Chatroom.vue
+++ ChatRoom/src/views/Chatroom.vue
@@ -27,9 +27,31 @@
   </div>
 </template>
 
+
+<script src="https://www.gstatic.com/firebasejs/8.5.0/firebase-app.js"></script>
+
 <script>
 import { mapState } from "vuex";
-import api from '@utils/request';
+//import api from '../utils/request.js';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA2QQEIj7kdjJgj0qNptnUG2Tl8ILC-LDU",
+    authDomain: "testing-purposes-4aaca.firebaseapp.com",
+    databaseURL: "https://testing-purposes-4aaca-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "testing-purposes-4aaca",
+    storageBucket: "testing-purposes-4aaca.appspot.com",
+    messagingSenderId: "469281703912",
+    appId: "1:469281703912:web:84f72377d61d663ca4065d",
+    measurementId: "G-01W01QKBNB"
+  };
+
+
+var FB =firebase.initializeApp(firebaseConfig);
+console.log(FB);
+var db = FB.database();
+const fetcheddata = db.ref().child('Kiryu');
+fetcheddata.on('value', snap =>console.log(snap.val()));
 
 export default {
 	name: "Chatroom",
@@ -61,9 +83,21 @@ export default {
   },
 	mehtods: {
 		initPaginatedItems() {
-      this.list = this.msgList;
+      this.list = this.msgList;		
     },
-	}
+		Trypush(){
+			var FB = startfirebase;
+			console.log(FB);
+			var db = FB.database();
+			var ref = db.ref('scores');
+			var data = {
+				name: "Amane Kanata",
+				message: "coco daisuki"
+			}
+			ref.push(data);
+		}
+	},
+
 }
 </script>
 
